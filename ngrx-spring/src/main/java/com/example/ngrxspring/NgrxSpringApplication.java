257b3fa06b3a732093f549b19bd218c0efb195b1
ashajyothig <ashajyothig@ashajyothiglp.ds.alacriti.com>
2021-10-12 21:46:41 +0530

    springboot project creation

---
+++ ngrx-spring/src/main/java/com/example/ngrxspring/NgrxSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngrxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgrxSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgrxSpringApplication.class, args);
+	}
+
+}
